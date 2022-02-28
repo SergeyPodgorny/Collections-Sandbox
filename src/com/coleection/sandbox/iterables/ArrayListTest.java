05d75b1115ac1382f2521c8af51b275cf4b0e2bf
SergeyPodgorny <serkonpod@gmail.com>
2022-02-28 13:03:09 +0300

    generics added

--- src/com/coleection/sandbox/iterables/ArrayListTest.java
+++ src/com/coleection/sandbox/iterables/ArrayListTest.java
@@ -1,5 +1,25 @@
 package com.coleection.sandbox.iterables;
 
+import java.util.ArrayList;
+
+
 public class ArrayListTest {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		ArrayList<?super Number> someList = new ArrayList<>(10);
+		
+		
+		someList.add((Double)5.0);
+		
+		
+		System.out.println(someList.isEmpty());
+		
+		System.out.println(someList.get(0));
+	}
+	
 
 }
